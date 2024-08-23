44439c8b2ee7e322b21eb3e74d9ee67cec297f2a
TranhungIT2003 <s3928533@rmit.edu.vn>
2024-08-23 22:37:38 +0700

    Initial Commit

---
+++ IOS-assignment-2-2024b-TranHungIT2003-Race-To-Avoid iOS/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  IOS-assignment-2-2024b-TranHungIT2003-Race-To-Avoid iOS
+//
+//  Created by MacBook Pro Của A Tú on 23/08/2024.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
